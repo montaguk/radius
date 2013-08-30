1c616346ac741162ffd401f1b26746b5b77fb633
Kyson <kyson@kyson-Thinkpad.(none)>
2013-08-29 23:26:12 -0700

    Adding google map v2.  Needs api key and app signature to work.
    This is needed cause map v1 is depricated

---
+++ Resources/alloy/controllers/index.js
@@ -0,0 +1,47 @@
+function Controller() {
+    function report(evt) {
+        Ti.API.info("Annotation " + evt.title + " clicked, id: " + evt.annotation.myid);
+    }
+    require("alloy/controllers/BaseController").apply(this, Array.prototype.slice.call(arguments));
+    this.__controllerPath = "index";
+    arguments[0] ? arguments[0]["__parentSymbol"] : null;
+    arguments[0] ? arguments[0]["$model"] : null;
+    arguments[0] ? arguments[0]["__itemTemplate"] : null;
+    var $ = this;
+    var exports = {};
+    var __defers = {};
+    $.__views.index = Ti.UI.createWindow({
+        id: "index"
+    });
+    $.__views.index && $.addTopLevelView($.__views.index);
+    $.__views.mapview = Alloy.Globals.Map.createView({
+        id: "mapview",
+        ns: "Alloy.Globals.Map"
+    });
+    $.__views.index.add($.__views.mapview);
+    report ? $.__views.mapview.on("click", report) : __defers["$.__views.mapview!click!report"] = true;
+    exports.destroy = function() {};
+    _.extend($, $.__views);
+    var mountainView = Alloy.Globals.Map.createAnnotation({
+        latitude: 37.390749,
+        longitude: -122.081651,
+        title: "Appcelerator Headquarters",
+        subtitle: "Mountain View, CA",
+        pincolor: Alloy.Globals.Map.ANNOTATION_RED,
+        myid: 1
+    });
+    $.mapview.region = {
+        latitude: 33.74511,
+        longitude: -84.38993,
+        latitudeDelta: .01,
+        longitudeDelta: .01
+    };
+    $.mapview.addAnnotation(mountainView);
+    $.index.open();
+    __defers["$.__views.mapview!click!report"] && $.__views.mapview.on("click", report);
+    _.extend($, exports);
+}
+
+var Alloy = require("alloy"), Backbone = Alloy.Backbone, _ = Alloy._;
+
+module.exports = Controller;
--- app/controllers/index.js
+++ app/controllers/index.js
@@ -1,5 +1,19 @@
-function doClick(e) {
-    alert($.label.text);
+function report(evt) {
+    Ti.API.info("Annotation " + evt.title + " clicked, id: " + evt.annotation.myid);
 }
+
+// API calls to the map module need to use the Alloy.Globals.Map reference
+var mountainView = Alloy.Globals.Map.createAnnotation({
+    latitude:37.390749,
+    longitude:-122.081651,
+    title:"Appcelerator Headquarters",
+    subtitle:'Mountain View, CA',
+    pincolor:Alloy.Globals.Map.ANNOTATION_RED,
+    myid:1 // Custom property to uniquely identify this annotation.
+});
+
+$.mapview.region = {latitude:33.74511, longitude:-84.38993,
+                    latitudeDelta:0.01, longitudeDelta:0.01};
+$.mapview.addAnnotation(mountainView);
 
 $.index.open();
